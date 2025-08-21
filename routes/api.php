7881e8048b4341a5fd7fab2c956ba50f65b60ce3
ian1219 <115201542+ian1219@users.noreply.github.com>
2025-08-22 03:36:25 +0800

    updated api

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 // Public Routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/user/register', [UserController::class, 'store']); // Public user registration
+Route::post('/role/register', [RoleController::class, 'store']); // Public role registration needs to create role first before creating a user
 Route::post('/user/read/{id}', [UserController::class, 'show']);
 Route::get('/roles', [RoleController::class, 'publicIndex']);
 
